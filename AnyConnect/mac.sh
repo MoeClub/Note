013f13006e2f0972c3b8e7237e0e6793b1b81aee
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-01-03 17:40:12 +0800

    Update mac.sh

--- AnyConnect/mac.sh
+++ AnyConnect/mac.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-CSET_PWD=""
-CSET_URL=""
+[[ $# -eq 2 ]] || exit 1
+CRET_URL="$1"
+CRET_PWD="$2"
+
 
 # DO NOT EDIT
+[[ -n "${CRET_URL}" ]] && [[ -n "${CRET_PWD}" ]] || exit 1
 USER_Home=`echo "$HOME"`
 [[ "$(sudo whoami)" == "root" ]] || exit 1
 
@@ -31,8 +34,8 @@ cat >"${USER_Home}/.anyconnect"<<EOF
 </AnyConnectPreferences>
 EOF
 
-curl -sSL -H "User-Agent: wget/1.0" -o "/tmp/MacOS.p12" "${CSET_URL}"
-[ -f "/tmp/MacOS.p12" ] && security import "/tmp/MacOS.p12" -P "${CSET_PWD}"
+[[ -f "${CRET_URL}" ]] && cp -f "${CRET_URL}" "/tmp/MacOS.p12" || curl -sSL -H "User-Agent: wget/1.0" -o "/tmp/MacOS.p12" "${CRET_URL}"
+[ -f "/tmp/MacOS.p12" ] && security import "/tmp/MacOS.p12" -P "${CRET_PWD}"
 [ -f "/tmp/MacOS.p12" ] && rm -rf "/tmp/MacOS.p12"
 
 
