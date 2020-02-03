44305358d44c1d09af6af34ba80d59ff5e828d4e
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-02-03 14:52:58 +0800

    Update UltraEdit.sh

--- UltraEdit/UltraEdit.sh
+++ UltraEdit/UltraEdit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#TMPDIR="$(echo $HOME)/Downloads/UltraEdit_v18.00.0.66"
-#rm -rf "${TMPDIR}"
-#mkdir -p "${TMPDIR}"
-#cd "${TMPDIR}"
+TMPDIR="$(echo $HOME)/Downloads/UltraEdit_v18.00.0.66"
+rm -rf "${TMPDIR}"
+mkdir -p "${TMPDIR}"
+cd "${TMPDIR}"
 
 # Download
-#for num in {1..6}; do
-#  FileName="UltraEdit_18.00.0.66.dmg.zip.00${num}"
-#  echo "Download: ${FileName}..."
-#  curl -sSL -o "${FileName}" "https://raw.githubusercontent.com/MoeClub/Note/master/UltraEdit/UltraEdit_18.00.0.66.dmg/${FileName}"
-#done
-#tar xvf <(cat UltraEdit_18.00.0.66.dmg.zip.001 UltraEdit_18.00.0.66.dmg.zip.002 UltraEdit_18.00.0.66.dmg.zip.003 UltraEdit_18.00.0.66.dmg.zip.004 UltraEdit_18.00.0.66.dmg.zip.005 UltraEdit_18.00.0.66.dmg.zip.006)
-#cp -rf UltraEdit_18.00.0.66.dmg ../
-#cd ../ && rm -rf "${TMPDIR}"
+for num in {1..6}; do
+  FileName="UltraEdit_18.00.0.66.dmg.zip.00${num}"
+  echo "Download: ${FileName}..."
+  curl -sSL -o "${FileName}" "https://raw.githubusercontent.com/MoeClub/Note/master/UltraEdit/UltraEdit_18.00.0.66.dmg/${FileName}"
+done
+tar xvf <(cat UltraEdit_18.00.0.66.dmg.zip.001 UltraEdit_18.00.0.66.dmg.zip.002 UltraEdit_18.00.0.66.dmg.zip.003 UltraEdit_18.00.0.66.dmg.zip.004 UltraEdit_18.00.0.66.dmg.zip.005 UltraEdit_18.00.0.66.dmg.zip.006)
+cp -rf UltraEdit_18.00.0.66.dmg ../
+cd ../ && rm -rf "${TMPDIR}"
 
 # Install
 hdiutil attach "UltraEdit_18.00.0.66.dmg" -nobrowse
