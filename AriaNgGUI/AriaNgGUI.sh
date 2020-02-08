9417ab23b71956a233bb081d3df6ec1ebf064adc
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-02-08 14:11:34 +0800

    Add files via upload

--- AriaNgGUI/AriaNgGUI.sh
+++ AriaNgGUI/AriaNgGUI.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+FileBaseName="AriaNgGUI.app.tar.gz"
 TMPDIR="$(echo $HOME)/Downloads/AriaNgGUI"
 rm -rf "${TMPDIR}"
 mkdir -p "${TMPDIR}"
 cd "${TMPDIR}"
 
-# Download
-for num in {1..10}; do
-  [ $num -lt 10 ] && FileName="AriaNgGUI.app.zip.00${num}"
-  [ $num -ge 10 ] && [ $num -lt 100 ] && FileName="AriaNgGUI.app.zip.0${num}"
-  [ $num -ge 100 ] && FileName="AriaNgGUI.app.zip.${num}"
+## Package
+#tar -zcvf ./AriaNgGUI.app.tar.gz ./AriaNgGUI.app
+#cat ./AriaNgGUI.app.tar.gz |split -b 20m -a 3 - AriaNgGUI.app.tar.gz.
+
+## Download
+
+for num in {1..4}; do
+  [ $num -lt 10 ] && FileName="${FileBaseName}.00${num}"
+  [ $num -ge 10 ] && [ $num -lt 100 ] && FileName="${FileBaseName}.0${num}"
+  [ $num -ge 100 ] && FileName="${FileBaseName}.${num}"
   echo "Download: ${FileName}..."
-  curl -sSL -o "${FileName}" "https://raw.githubusercontent.com/MoeClub/Note/master/AriaNgGUI/AriaNgGUI/${FileName}"
+  curl -sSL -o "${FileName}" "https://raw.githubusercontent.com/MoeClub/Note/master/AriaNgGUI/AriaNgGUI.app/${FileName}"
 done
 
-# Install
-tar xvf <(cat AriaNgGUI.app.zip.001 AriaNgGUI.app.zip.002 AriaNgGUI.app.zip.003 AriaNgGUI.app.zip.004 AriaNgGUI.app.zip.005 AriaNgGUI.app.zip.006 AriaNgGUI.app.zip.007 AriaNgGUI.app.zip.008 AriaNgGUI.app.zip.009 AriaNgGUI.app.zip.010) -C "/Applications"
+## Install
+[ -e "$HOME/Library/Application Support/aria-ng-gui" ] && rm -rf "$HOME/Library/Application Support/aria-ng-gui"
+[ -e "/Applications/AriaNgGUI.app" ] && rm -rf "/Applications/AriaNgGUI.app"
+tar xvf <(cat AriaNgGUI.app.tar.gz.001 AriaNgGUI.app.tar.gz.002 AriaNgGUI.app.tar.gz.003 AriaNgGUI.app.tar.gz.004) -C "/Applications"
 cd ../ && rm -rf "${TMPDIR}"
 
