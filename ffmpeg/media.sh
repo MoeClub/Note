06703f54d0173d41805efa3d608065b6ce93e80e
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-02-23 21:12:13 +0800

    Update media.sh

--- ffmpeg/media.sh
+++ ffmpeg/media.sh
@@ -39,7 +39,8 @@ if [ "$ForceH264" -eq 0 ]; then
 fi
 if [ "$ForceH264" -ne 0 ]; then
   ForceMaxRate=`awk 'BEGIN{print '${ForceRate}' * '${ForceMaxRadio}'}' |cut -d'.' -f1`
-  VideoAddon="-b:v ${ForceRate} -maxrate ${ForceMaxRate} -bufsize ${ForceMaxRate}"
+  ForceBuf=`awk 'BEGIN{print '${ForceRate}' / '${ForceMaxRadio}'}' |cut -d'.' -f1`
+  VideoAddon="-b:v ${ForceRate} -maxrate ${ForceMaxRate} -bufsize ${ForceBuf}"
   VideoCode="h264"
   if [ "$BitRate" -gt "3500000" ]; then
     BitRadio='${ForceBitRadio}'
@@ -65,7 +66,8 @@ else
     fi
     echo "media bitrate(new): ${BitRate}"
     ForceMaxRate=`awk 'BEGIN{print '${ForceRate}' * '${ForceMaxRadio}'}' |cut -d'.' -f1`
-    VideoAddon="-b:v ${ForceRate} -maxrate ${ForceMaxRate} -bufsize ${ForceMaxRate}"
+    ForceBuf=`awk 'BEGIN{print '${ForceRate}' / '${ForceMaxRadio}'}' |cut -d'.' -f1`
+    VideoAddon="-b:v ${ForceRate} -maxrate ${ForceMaxRate} -bufsize ${ForceBuf}"
   fi
 fi
 VideoTime=`awk 'BEGIN{print ('${MaxSize}' * 1024 * 1024 * 8) / ('${BitRate}' * '${BitRadio}') }' |cut -d'.' -f1`
