8fb1b8236dd09d643098b2d4e3ec5694e9473bd8
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-03-12 10:28:01 +0800

    Add files via upload

---
+++ AnyConnect/redirect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HostName="server.host"
+HostPort="443"
+LocalPort="443"
+LocalIf="eth0"
+
+RemoteHost=`curl -o /dev/null -sSL --connect-timeout 5 --retry-delay 3 --retry 5 -w %{remote_ip} "https://${HostName}:${HostPort}"`
+[ -n "$RemoteHost" ] || exit 1
+
+Forward=`cat /proc/sys/net/ipv4/ip_forward`
+[ "$Forward" != "1" ] && echo "1" >/proc/sys/net/ipv4/ip_forward
+
+iptables -I INPUT -p tcp --dport ${LocalPort} -j ACCEPT
+iptables -t nat -A PREROUTING -p tcp -i ${LocalIf} --dport ${LocalPort} -j DNAT --to-destination ${RemoteHost}:${HostPort}
+iptables -t nat -I POSTROUTING -d ${RemoteHost} -p tcp --dport ${HostPort} -j MASQUERADE
+
