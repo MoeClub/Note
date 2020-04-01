b98addd287ee4a831573323ddfdb59810bba86c3
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-04-01 17:53:53 +0800

    Update Install.sh

--- ffmpeg/Install.sh
+++ ffmpeg/Install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# bash Install.sh <yuque_ctoken> <yuque_session>
+# bash Install.sh
 
-yuque_ctoken="${1:-}"
-yuque_session="${2:-}"
-
-[ -n "$yuque_ctoken" ] && [ -n "$yuque_session" ] || exit 1
 ItemURL="https://raw.githubusercontent.com/MoeClub/Note/master/ffmpeg/"
-FileList=("upload_yuque.sh" "publish.sh" "media.sh" "m3u8.sh" "Player/Player.html" "Player/Player.py" "Player/static/css/video-js.min.css" "Player/static/js/video.min.js" "Player/static/js/videojs.hotkeys.min.js")
+FileList=("upload.sh" "publish.sh" "media.sh" "m3u8.sh" "Player/Player.html" "Player/Player.py" "Player/static/css/video-js.min.css" "Player/static/js/video.min.js" "Player/static/js/videojs.hotkeys.min.js")
 
 for Item in "${FileList[@]}"; do
   echo "Download ${Item} ..."
@@ -17,9 +13,5 @@ for Item in "${FileList[@]}"; do
   chmod 755 "${Item}"
 done
 
-sed -i "s|^ctoken=.*|ctoken=\"$yuque_ctoken\"|" upload_yuque.sh
-sed -i "s|^session=.*|session=\"$yuque_session\"|" upload_yuque.sh
-cp -rf upload_yuque.sh upload.sh
-chmod 755 upload.sh
 mkdir -p "Player/data"
 chmod 755 "Player/data"
