c5b24eddefaf5008ca1f595d362fd94bfdab81c9
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-08-30 02:43:07 +0800

    Create test_v1.sh

---
+++ AnyConnect/build/test_v1.sh
@@ -0,0 +1,12 @@
+cd /tmp && wget ftp://ftp.infradead.org/pub/ocserv/ocserv-1.1.0.tar.xz -O ocserv.tar.xz
+
+cd /tmp
+[ -d ocserv ] && rm -rf ocserv
+mkdir ocserv; tar -xJ -f ocserv.tar.xz -C ocserv --strip-components=1;
+cd ocserv
+
+LIBGNUTLS_LIBS="-llibgnutls" \
+CFLAGS="-I." \
+LDFLAGS="-L. -static" \
+./configure --without-{protobuf,pam,radius,http-parser,lz4,gssapi,pcl-lib}
+
