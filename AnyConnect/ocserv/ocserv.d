4ec2b44df64c5169ad553ff0718cfd8655c6ce41
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-08-31 20:48:07 +0800

    Update ocserv.d

--- AnyConnect/ocserv/ocserv.d
+++ AnyConnect/ocserv/ocserv.d
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-MyPath="$(dirname `readlink -f "$0"`)"
-MyPort="$(cat ${MyPath}/ocserv.conf |grep '^tcp-port' |grep -o '[0-9]*')"
-MyStartUp="/etc/init.d/ocserv"
-[ -e "$(which nc)" ] || exit 1
-[ -e ${MyStartUp} ] || exit 1
 
-PORT_STATUS(){
+[ -e "$(which nc)" ] || exit 1
+MyPath="$(dirname `readlink -f "$0"`)"
+MyConfig="${MyPath}/ocserv.conf"
+[ -f "${MyConfig}" ] || exit 1
+MyPort=`cat "${MyConfig}" |grep '^tcp-port' |grep -o '[0-9]*'`
+
+
+START(){
+  ocserv --config "${MyConfig}" >/dev/null 2>&1
+}
+
+PORT(){
   nc -w 1 -vz 0.0.0.0 ${MyPort} >/dev/null 2>&1
   [[ "$?" == "0" ]] && echo "0" || echo "1"
 }
 
 SCAN(){
-  if [[ "$(PORT_STATUS)" == "0" ]]; then
+  if [[ "$(PORT)" == "0" ]]; then
     sleep 300;
   else
     kill -9 <(ps -C ocserv -o pid=) 1>/dev/null 2>&1
-    bash ${MyStartUp} restart 1>/dev/null 2>&1 &
+    START;
     sleep 10;
   fi
 }
 
 while true; do SCAN; done
+
