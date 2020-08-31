6d32fc8fd43ac3ed02c72b9993886a3c5b63abd9
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-08-31 15:14:16 +0800

    Update static_ocserv.sh

--- AnyConnect/build/static_ocserv.sh
+++ AnyConnect/build/static_ocserv.sh
@@ -4,10 +4,10 @@ cd /tmp
 #################
 #################
 
-ver_nettle=3.4.1
-ver_gnutls=3.6.7
-ver_libev=4.25
-ver_ocserv=1.1.0
+ver_nettle=3.3
+ver_gnutls=3.5.8
+ver_libev=4.22
+ver_ocserv=0.12.6
 
 #################
 #################
@@ -15,51 +15,79 @@ ver_ocserv=1.1.0
 cores=$(grep -c '^processor' /proc/cpuinfo)
 instPrefix="/tmp/inst"
 
+export CC=/usr/bin/gcc
+export PKG_CONFIG_SYSROOT_DIR="$instPrefix"
+export PKG_CONFIG_LIBDIR="$instPrefix/lib/pkgconfig"
+
+
+#################
+#################
+
 rm -rf $instPrefix
 mkdir -p $instPrefix
 ln -s . $instPrefix/usr
 ln -s . $instPrefix/local
 
-export CC=/usr/bin/gcc
-export PKG_CONFIG_SYSROOT_DIR="$instPrefix"
-export PKG_CONFIG_LIBDIR="$instPrefix/lib/pkgconfig"
+#################
+#################
 
 # Nettle
 wget --no-check-certificate -4 -O nettle.tar.gz https://ftp.gnu.org/gnu/nettle/nettle-${ver_nettle}.tar.gz
 [ -d nettle ] && rm -rf nettle
 mkdir -p nettle; tar -xz -f nettle.tar.gz -C nettle --strip-components=1;
 cd nettle
+cat >>mini-gmp.c <<EOF
+void mpz_div_2exp(mpz_t quotient, mpz_t dividend, unsigned long int exponent_of_2)
+{
+	mpz_tdiv_q_2exp(quotient, dividend, exponent_of_2);
+}
+void mpz_mod_2exp(mpz_t remainder, mpz_t dividend, unsigned long int exponent_of_2)
+{
+	mpz_tdiv_r_2exp(remainder, dividend, exponent_of_2);
+}
+EOF
+CFLAGS="-I$instPrefix/include" LDFLAGS="-L$instPrefix/lib -O2 -fPIC" \
 ./configure \
-	--enable-mini-gmp --enable-x86-aesni \
-	--disable-{documentation,shared}
-sed -i 's/cnd-copy\.c /&cnd-memcpy.c /' Makefile
+	--enable-mini-gmp --enable-x86-aesni --enable-static \
+	--disable-{documentation,shared,rpath}
+#sed -i 's/cnd-copy\.c /&cnd-memcpy.c /' Makefile
 make -j$cores
+[ $? -eq 0 ] || exit 1 
 make DESTDIR=$instPrefix install
 cd ..
+
 
 # GnuTLS
 wget --no-check-certificate -4 -O gnutls.tar.xz ftp://ftp.gnutls.org/gcrypt/gnutls/v${ver_gnutls%.*}/gnutls-${ver_gnutls}.tar.xz
 [ -d gnutls ] && rm -rf gnutls
 mkdir -p gnutls; tar -xJ -f gnutls.tar.xz -C gnutls --strip-components=1;
 cd gnutls
-CFLAGS=-I$instPrefix/include LDFLAGS=-L$instPrefix/lib \
+sed -i '/gmp\.h/d' lib/nettle/int/dsa-fips.h
+CFLAGS="-I$instPrefix/include" LDFLAGS="-L$instPrefix/lib -O2 -fPIC" \
 ./configure \
 	--with-nettle-mini --with-included-{libtasn1,unistring} \
-	--without-p11-kit --disable-shared \
-	--disable-{doc,tools,cxx,tests,nls,guile}
+	--without-p11-kit --enable-static \
+	--disable-{doc,tools,cxx,tests,nls,guile,rpath,shared}
 make -j$cores
+[ $? -eq 0 ] || exit 1 
 make DESTDIR=$instPrefix install
 cd ..
+
 
 # libev
 wget --no-check-certificate -4 -O libev.tar.gz http://dist.schmorp.de/libev/Attic/libev-${ver_libev}.tar.gz
 [ -d libev ] && rm -rf libev
 mkdir -p libev; tar -xz -f libev.tar.gz -C libev --strip-components=1;
 cd libev
-./configure --disable-shared
+CFLAGS="-I$instPrefix/include" LDFLAGS="-L$instPrefix/lib -O2 -fPIC" \
+./configure \
+  --enable-static \
+	--disable-{shared,rpath} 
 make -j$cores
+[ $? -eq 0 ] || exit 1 
 make DESTDIR=$instPrefix install
 cd ..
+
 
 # readline.h
 cat >$instPrefix/include/readline.h <<EOF
@@ -105,6 +133,7 @@ EOF
 ar rcs $instPrefix/lib/libreadline.a readline.o
 rm -rf readline.o
 
+
 # OpenConnect server
 rm -rf $HOME/ocserv-bin
 mkdir -p $HOME/ocserv-bin
@@ -112,10 +141,11 @@ wget --no-check-certificate -4 -O ocserv.tar.xz ftp://ftp.infradead.org/pub/ocse
 [ -d ocserv ] && rm -rf ocserv
 mkdir -p ocserv; tar -xJ -f ocserv.tar.xz -C ocserv --strip-components=1;
 cd ocserv
-autoreconf -fvi
+#autoreconf -fvi
+sed -i 's/#define DEFAULT_CONFIG_ENTRIES 96/#define DEFAULT_CONFIG_ENTRIES 200/' src/vpn.h
 sed -i 's/\$LIBS \$LIBEV/\$LIBEV \$LIBS/g' configure
 CFLAGS="-I$instPrefix/include" \
-LDFLAGS="-L$instPrefix/lib -fPIC -static -pthread -lpthread" \
+LDFLAGS="-L$instPrefix/lib -O2 -fPIC -static -pthread -lpthread" \
 LIBNETTLE_LIBS="-lnettle -lhogweed" LIBREADLINE_LIBS="-lreadline" \
 LIBS="-lm" \
 ./configure --prefix=/usr \
@@ -124,6 +154,10 @@ LIBS="-lm" \
 	--without-{root-tests,docker-tests,nuttcp-tests} \
 	--without-{protobuf,maxmind,geoip,liboath,pam,radius,utmp,lz4,http-parser,gssapi,pcl-lib}
 make -j$cores
+[ $? -eq 0 ] || exit 1 
 make DESTDIR=$HOME/ocserv-bin install
 cd ..
 
+# cd $HOME/ocserv-bin
+# tar -cvf "../ocserv_v0.12.6.tar" ./
+# tar --overwrite -xvf ocserv_v0.12.6.tar -C /
