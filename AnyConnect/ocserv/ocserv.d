ebe06c3739d2f798968363206962bf3282f7684a
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-08-31 21:45:19 +0800

    Update ocserv.d

--- AnyConnect/ocserv/ocserv.d
+++ AnyConnect/ocserv/ocserv.d
@@ -9,19 +9,20 @@ MyPort=`cat "${MyConfig}" |grep '^tcp-port' |grep -o '[0-9]*'`
 
 
 START(){
-  ocserv --config "${MyConfig}" >>/dev/null 2>&1
+  kill -9 $(ps -C ocserv -o pid=) >>/dev/null 2>&1;
+  kill -9 $(ps -C ocserv-main -o pid=) >>/dev/null 2>&1;
+  ocserv --config "${MyConfig}" >>/dev/null 2>&1;
 }
 
 PORT(){
-  nc -w 1 -vz 0.0.0.0 ${MyPort} >>/dev/null 2>&1
-  [[ "$?" == "0" ]] && echo "0" || echo "1"
+  nc -w 1 -vz 0.0.0.0 ${MyPort} >>/dev/null 2>&1;
+  [[ "$?" == "0" ]] && echo "0" || echo "1";
 }
 
 SCAN(){
   if [[ "$(PORT)" == "0" ]]; then
     sleep 300;
   else
-    kill -9 <(ps -C ocserv -o pid=) >>/dev/null 2>&1
     START;
     sleep 10;
   fi
