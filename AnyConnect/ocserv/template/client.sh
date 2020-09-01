84beeb52b43ddbb224025ce30a93a27927c579da
MoeClub <32381827+MoeClub@users.noreply.github.com>
2020-09-01 18:20:38 +0800

    Update client.sh

--- AnyConnect/ocserv/template/client.sh
+++ AnyConnect/ocserv/template/client.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
-FriendlyName="AnyConnect"
-CA="Moeclub"
 
-[ $# -eq '1' ] && PASSWORD="$1";
-command -v openssl >>/dev/null 2>&1
-[ $? -ne 0 ] && echo "Not Found `openssl`" && exit 1
 command -v certtool >>/dev/null 2>&1
 [ $? -ne 0 ] && echo "Not Found `certtool`" && exit 1
 
+export OrgName
+export GroupName
+export PASSWORD
 
-Check() {
-  if [ ! -f "./$1" ]; then
-    echo "Not Found $1"
-    exit 1
+
+while [[ $# -ge 1 ]]; do
+  case $1 in
+    -o)
+      shift
+      OrgName=`echo "$1" |sed 's/\s//g'`
+      shift
+      ;;
+    -g)
+      shift
+      GroupName=`echo "$1" |sed 's/\s//g'`
+      shift
+      ;;
+    -p)
+      shift
+      PASSWORD=`echo "$1" |sed 's/\s//g'`
+      shift
+      ;;
+    *)
+      echo "bash $0 -o <OrgName> -g <GroupName> -p <PASSWORD>"
+      exit 1;
+      ;;
+  esac
+done
+
+[ -n "${OrgName}" ] || OrgName="Haibara"
+[ -n "${GroupName}" ] || GroupName="Default"
+
+
+if [ ! -f ./ca.cert.pem -o ! -f ./ca.key.pem ]; then
+  if [ ! -f ./ca.tmpl ]; then
+    echo -e "cn = \"${OrgName} CA\"\norganization = \"${OrgName}\"\nserial = 1\nexpiration_days = 3650\nca\nsigning_key\ncert_signing_key\ncrl_signing_key\n" >./ca.tmpl
   fi
-}
+  certtool --generate-privkey --outfile ./ca.key.pem
+  certtool --generate-self-signed --template ./ca.tmpl --load-privkey ./ca.key.pem --outfile ./ca.cert.pem
+  cp -rf ./ca.cert.pem ../ca.cert.pem
+fi
 
-Remove() {
-  [ -n "$1" ] || exit 1
-  if [ -f "./$1" ]; then
-    rm -rf "./$1"
-  fi
-}
+echo "cn = \"${OrgName}.${GroupName}\"\nunit = \"${GroupName}\"\nexpiration_days = 3650\nsigning_key\ntls_www_client\n" >user.tmpl
+certtool --generate-privkey --outfile ./user.key.pem
+certtool --generate-certificate --hash SHA256 --load-privkey ./user.key.pem --load-ca-certificate ./ca.cert.pem --load-ca-privkey ./ca.key.pem --template ./user.tmpl --outfile ./user.cert.pem
+cat ./ca.cert.pem >>./user.cert.pem
+certtool --to-p12 --pkcs-cipher 3des-pkcs12 --load-privkey ./user.key.pem --load-certificate ./user.cert.pem --p12-name="${OrgName}.${GroupName}" --outfile "./${GroupName}.p12" --outder --empty-password --password=$PASSWORD;
 
-## Generate CA
-# certtool --generate-privkey --outfile ./ca-key.pem
-# certtool --generate-self-signed --load-privkey ./ca-key.pem --template ./ca.tmpl --outfile ./ca-cert.pem
-
-Check "ca-cert.pem"
-Check "ca-key.pem"
-Check "user.tmpl"
-Remove "user-key.pem"
-Remove "user-cert.pem"
-GROUP=`sed -n '/^unit/p' user.tmpl |cut -d'"' -f2`
-[ ! -n "$GROUP" ] && echo "No Group." && exit 1
-certtool --generate-privkey --outfile ./user-key.pem
-certtool --generate-certificate --hash SHA256 --load-privkey ./user-key.pem --load-ca-certificate ./ca-cert.pem --load-ca-privkey ./ca-key.pem --template ./user.tmpl --outfile ./user-cert.pem
-cat ./ca-cert.pem >>./user-cert.pem
-openssl pkcs12 -export -inkey ./user-key.pem -in ./user-cert.pem -name "${FriendlyName}.${GROUP}" -certfile ./ca-cert.pem -caname "${CA}" -out "./${GROUP}.p12" -passout pass:$PASSWORD
-[ $? -eq '0' ] && echo -e "\nSuccess! \nGROUP\t\tPASSWORD\n$GROUP\t\t$PASSWORD\n" || echo -e "\nFail! \n";
+[ $? -eq '0' ] && echo -e "\nSuccess! \nGROUP\t\tPASSWORD\n${GroupName}\t\t$PASSWORD\n" || echo -e "\nFail! \n";
