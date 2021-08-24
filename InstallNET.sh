727d4d2d9f21b512c9c87bb1a8c640b76d1bd453
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-08-24 15:42:02 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -248,8 +248,8 @@ function getGrub(){
   if [ -z "$fileName" ]; then
     ls -1 "$folder" 2>/dev/null |grep -q '^grubenv$'
     [ $? -eq 0 ] || return
-    grubEnv=`readlink -f "${folder}/grubenv"`
-    folder=`dirname "$grubEnv"`
+    folder=`find "$Boot" -type f -name "grubenv" 2>/dev/null |xargs dirname |grep -v "$folder" |head -n1`
+    [ -n "$folder" ] || return
     fileName=`ls -1 "$folder" 2>/dev/null |grep '^grub.conf$\|^grub.cfg$'`
   fi
   [ -n "$fileName" ] || return
