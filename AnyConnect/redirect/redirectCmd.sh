273b597e47c7e13454af4d0b9dd3ca708115af77
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-09-10 17:50:20 +0800

    Update redirectCmd.sh

--- AnyConnect/redirect/redirectCmd.sh
+++ AnyConnect/redirect/redirectCmd.sh
@@ -2,14 +2,28 @@
 
 RemoteAddress="${1:-}"
 LocalPort="${2:-443}"
-LocalIf="${3:-eth0}"
+LocalIf="${3:-}"
+
+function getInterface(){
+  interface=""
+  Interfaces=`cat /proc/net/dev |grep ':' |cut -d':' -f1 |sed 's/\s//g' |grep -iv '^lo\|^sit\|^stf\|^gif\|^dummy\|^vmnet\|^vir\|^gre\|^ipip\|^ppp\|^bond\|^tun\|^tap\|^ip6gre\|^ip6tnl\|^teql\|^ocserv\|^vpn'`
+  defaultRoute=`ip route show default |grep "^default"`
+  for item in `echo "$Interfaces"`
+    do
+      [ -n "$item" ] || continue
+      echo "$defaultRoute" |grep -q "$item"
+      [ $? -eq 0 ] && interface="$item" && break
+    done
+  echo "$interface"
+}
 
 echo "${RemoteAddress}" |grep -q "[0-9a-zA-Z\.]\+:[0-9]\{1,5\}"
 [ "$?" -ne 0 ] && echo "Invalid RemoteAddress Host:Port" && exit 1
+[ -n "$LocalIf" ] || LocalIf="$(getInterface)"
 
-Forward=`cat /proc/sys/net/ipv4/ip_forward`
-[ "$Forward" != "1" ] && echo "1" >/proc/sys/net/ipv4/ip_forward
+[ "$(cat /proc/sys/net/ipv4/ip_forward)" != "1" ] && echo "1" >/proc/sys/net/ipv4/ip_forward
 
 iptables -I INPUT -p tcp --dport ${LocalPort} -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp -i ${LocalIf} --dport ${LocalPort} -j DNAT --to-destination ${RemoteAddress}
 iptables -t nat -I POSTROUTING -d ${RemoteHost} -p tcp --dport ${HostPort} -j MASQUERADE
+
