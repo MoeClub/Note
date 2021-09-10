2b24d71d24c82bff314e08da8eea9911c451bcfc
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-09-10 18:01:16 +0800

    Update redirectCmd.sh

--- AnyConnect/redirect/redirectCmd.sh
+++ AnyConnect/redirect/redirectCmd.sh
@@ -18,12 +18,17 @@ function getInterface(){
 }
 
 echo "${RemoteAddress}" |grep -q "[0-9a-zA-Z\.]\+:[0-9]\{1,5\}"
-[ "$?" -ne 0 ] && echo "Invalid RemoteAddress Host:Port" && exit 1
+[ "$?" -ne 0 ] && echo "Invalid RemoteAddress(Host:Port)" && exit 1
+RemoteHost="$(host $(echo ${RemoteAddress} |cut -d: -f1) |grep -o '[0-9\.]\{1,3\}\.[0-9\.]\{1,3\}\.[0-9\.]\{1,3\}\.[0-9\.]\{1,3\}')"
+RemotePort="$(echo ${RemoteAddress} |cut -d: -f2 |grep -o '[0-9]\{1,5\}')"
+
+[ ! -n "${RemoteHost}" ] && echo "Invalid RemoteHost" && exit 1
+
 [ -n "$LocalIf" ] || LocalIf="$(getInterface)"
 
 [ "$(cat /proc/sys/net/ipv4/ip_forward)" != "1" ] && echo "1" >/proc/sys/net/ipv4/ip_forward
 
 iptables -I INPUT -p tcp --dport ${LocalPort} -j ACCEPT
-iptables -t nat -A PREROUTING -p tcp -i ${LocalIf} --dport ${LocalPort} -j DNAT --to-destination ${RemoteAddress}
+iptables -t nat -A PREROUTING -p tcp -i ${LocalIf} --dport ${LocalPort} -j DNAT --to-destination ${RemoteHost}:${RemotePort}
 iptables -t nat -I POSTROUTING -d ${RemoteHost} -p tcp --dport ${HostPort} -j MASQUERADE
 
