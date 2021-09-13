6e91f81d14222580775891ed58a6ae4c001ae090
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-09-13 12:55:47 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -538,7 +538,7 @@ if [[ "$loaderMode" == "0" ]]; then
   [[ "$setInterfaceName" == "1" ]] && Add_OPTION="net.ifnames=0 biosdevname=0" || Add_OPTION=""
   [[ "$setIPv6" == "1" ]] && Add_OPTION="$Add_OPTION ipv6.disable=1"
   
-  lowMem || Add_OPTION="$Add_OPTION lowmem=1"
+  lowMem || Add_OPTION="$Add_OPTION lowmem/low=1"
 
   if [[ "$linux_relese" == 'debian' ]] || [[ "$linux_relese" == 'ubuntu' ]]; then
     BOOT_OPTION="auto=true $Add_OPTION hostname=$linux_relese domain= -- quiet"
