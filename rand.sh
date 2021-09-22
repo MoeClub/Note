737c099ec2452d38157fe39e8971ca149b3b6b59
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-09-22 22:33:44 +0800

    Create rand.sh

---
+++ rand.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Min=65
+Max=95
+Num=0
+
+CORES=$(cat /proc/cpuinfo| grep -i "physical id"| sort| uniq| wc -l)
+
+function RAND() {
+  min=$1
+  max=$(($2-$min+1))
+  num=`cat /dev/urandom |head -n10 |cksum |grep -o '[0-9]*' |head -n1`
+  echo $(($num%$max+$min))
+}
+
+for((i=0;i<$CORES;i++)) do Num=$(($Num+$(RAND $Min $Max))); done
+
+echo $Num
