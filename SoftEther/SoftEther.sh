3f7b025ec166743a920d2787419703399f9ec300
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-12-10 17:14:07 +0800

    Update SoftEther.sh

--- SoftEther/SoftEther.sh
+++ SoftEther/SoftEther.sh
@@ -4,6 +4,8 @@
 export ADMIN=''
 export USER=''
 export PASSWD=''
+export SoftEtherURL=''
+export SoftEtherVersion='v4.38-9760-rtm-2021.08.17'
 
 while [[ $# -ge 1 ]]; do
   case $1 in
@@ -22,6 +24,11 @@ while [[ $# -ge 1 ]]; do
       PASSWD=`echo "$1" |sed 's/[[:space:]]//g'`
       shift
       ;;
+    -url)
+      shift
+      SoftEtherURL=`echo "$1" |sed 's/[[:space:]]//g'`
+      shift
+      ;;
     *)
       echo -ne " Usage:\n\tbash $(basename $0)\t-a [AdminPasswd] -u [User] -p [Passwd]\n"
       exit 1;
@@ -33,13 +40,21 @@ while [[ $# -ge 1 ]]; do
 [ -n "$USER" ] || USER="vpn"
 [ -n "$PASSWD" ] || PASSWD="vpn"
 
+case `uname -m` in aarch64|arm64) ARCH="arm64";; x86|i386|i686) ARCH="i386";; x86_64|amd64) ARCH="amd64";; *) ARCH="";; esac
+[ -n "$ARCH" ] || exit 1
+
+[ ! -n "$SoftEtherURL" ] && [ "$ARCH" == "arm64" ] && SoftEtherURL="http://www.softether-download.com/files/softether/${SoftEtherVersion}-tree/Linux/SoftEther_VPN_Server/64bit_-_ARM_64bit/softether-vpnserver-${SoftEtherVersion}-linux-arm64-64bit.tar.gz";
+[ ! -n "$SoftEtherURL" ] && [ "$ARCH" == "amd64" ] && SoftEtherURL="http://www.softether-download.com/files/softether/${SoftEtherVersion}-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-${SoftEtherVersion}-linux-x64-64bit.tar.gz";
+[ ! -n "$SoftEtherURL" ] && [ "$ARCH" == "i386" ] && SoftEtherURL="http://www.softether-download.com/files/softether/${SoftEtherVersion}-tree/Linux/SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-${SoftEtherVersion}-linux-x86-32bit.tar.gz";
+[ ! -n "$SoftEtherURL" ] && exit 1
 
 kill -9 `ps -C "vpnserver" -o pid=` >/dev/null 2>&1
 rm -rf /tmp/vpnserver
 rm -rf /etc/softether
 mkdir -p /etc/softether
 
-wget --no-check-certificate --no-cache -4 -qO "/tmp/softether.tar.gz" "https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz"
+
+wget --no-check-certificate --no-cache -4 -qO "/tmp/softether.tar.gz" "${SoftEtherURL}"
 wget --no-check-certificate --no-cache -4 -qO "/etc/softether/vpn_server.config" "https://raw.githubusercontent.com/MoeClub/Note/master/SoftEther/vpn_server.config"
 
 
