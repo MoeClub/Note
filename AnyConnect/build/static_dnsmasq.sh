42991d2d942995c4d43244863bce68160b43b633
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-03-11 22:48:25 +0800

    Update static_dnsmasq.sh

--- AnyConnect/build/static_dnsmasq.sh
+++ AnyConnect/build/static_dnsmasq.sh
@@ -9,10 +9,10 @@ wget --no-check-certificate -4 -O dnsmasq.tar.gz "http://www.thekelleys.org.uk/d
 mkdir -p dnsmasq; tar -xvf dnsmasq.tar.gz -C dnsmasq --strip-components=1;
 cd dnsmasq
 
-# Disable IPv6
+# Disable type 28 (IPv6)
 sed -i 's/^[[:space:]]*if (flags || ede == EDE_NOT_READY)[[:space:]]*$/      if (\!flags \&\& (gotname \& F_IPV6))\n        flags = F_NXDOMAIN;\n\n&/' ./src/forward.c
 
-# Disable type 65
+# Disable type 65 (HTTPS)
 sed -i 's/^#define T_CAA.*/&\n#define T_HTTPS\t\t65/' ./src/dns-protocol.h
 sed -i 's/^#define F_SRV.*/&\n#define F_HTTPS\t\t(1u<<31)/' ./src/dnsmasq.h
 sed -i "$((`sed -n -e '/^[[:space:]]*if (qtype == T_AAAA)/=' ./src/rfc1035.c` + 1))s/return F_IPV6;/return F_IPV6;\n      if (qtype == T_HTTPS)\n  return F_HTTPS;/" ./src/rfc1035.c
