2289b03541f60f9185a90a249fd164b7ff8bf6f5
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-03-11 16:50:19 +0800

    Update static_dnsmasq.sh

--- AnyConnect/build/static_dnsmasq.sh
+++ AnyConnect/build/static_dnsmasq.sh
@@ -2,10 +2,13 @@
 
 cd /tmp
 
-wget --no-check-certificate -4 -O dnsmasq.tar.xz http://www.thekelleys.org.uk/dnsmasq/dnsmasq-2.82.tar.xz
+wget --no-check-certificate -4 -O dnsmasq.tar.xz http://www.thekelleys.org.uk/dnsmasq/dnsmasq-2.86.tar.xz
 [ -d dnsmasq ] && rm -rf dnsmasq
 mkdir -p dnsmasq; tar -xJ -f dnsmasq.tar.xz -C dnsmasq --strip-components=1;
 cd dnsmasq
 
+# Disable IPv6
+sed -i "$((`sed -n -e '/^[[:space:]]*if (qtype == T_AAAA)/=' ./src/rfc1035.c` + 1))s/return F_IPV6;/return 0;\n      if \(qtype == 65\)\n	return 0;/" ./src/rfc1035.c
+
 make CFLAGS="-I. -Wall -W -fPIC -O2" LDFLAGS="-L. -static -s"
-make PREFIX="/usr" DESTDIR=.. install
+make PREFIX=/usr DESTDIR=/tmp install
