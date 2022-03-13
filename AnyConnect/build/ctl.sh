eb0cc4cafa0eb31efa17d061a338a8ed92bbb08d
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-03-13 15:38:03 +0800

    Add files via upload

--- AnyConnect/build/ctl.sh
+++ AnyConnect/build/ctl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ARG=`echo "$1" |sed 's/^\s$//' |sed 's/[a-z]/\u&/g'`
-PID=`echo "$2" |grep -o '[0-9]*' |head -n1`
 
 ConfigPath="$(dirname `readlink -f "$0"`)"
 Config="${ConfigPath}/ocserv.conf"
@@ -10,20 +9,9 @@ Config="${ConfigPath}/ocserv.conf"
 TCP=`cat "${Config}" |grep '^#\?tcp-port' |cut -d"=" -f2 |grep -o '[0-9]*' |head -n1`
 UDP=`cat "${Config}" |grep '^#\?udp-port' |cut -d"=" -f2 |grep -o '[0-9]*' |head -n1`
 
-if [ "$ARG" == "CHECK" ]; then
-  TCPHEX=`printf '%04X\n' "${TCP}"`
-  cat /proc/net/tcp |grep -q "^\s*[0-9]\+:\s*[0-9A-Za-z]\+:${TCPHEX}\s*[0-9A-Za-z]\+:[0-9A-Za-z]\+\s*0A\s*"
-  [ "$?" -eq 0 ] && exit 0 || exit 1
-fi
-
-Ether=`ip route show default |sed 's/.*dev\s*\([0-9a-zA-Z]\+\).*/\1/g'`
-[ -n "$Ether" ] || exit 1
-
-if [ -f "${ConfigPath}/group/NoRoute" ]; then
-  Address=`wget --no-check-certificate --timeout=3 --no-cache -4 -qO- "http://checkip.amazonaws.com" |grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' |head -n1`
-  [ -n "$Address" ] &&  sed -i "s/^no-route\s*=\s*.*\/255.255.255.255/no-route = ${Address}\/255.255.255.255/" "${ConfigPath}/group/NoRoute"
-fi
-
+function GetAddress(){
+  echo `wget --no-check-certificate --timeout=3 --no-cache -4 -qO- "http://checkip.amazonaws.com" |grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' |head -n1`
+}
 
 function IPTABLES(){
   RULE=`echo "$1" |sed 's/^\s*//' |sed 's/\s*$//'`
@@ -35,6 +23,31 @@ function IPTABLES(){
   return 0
 }
 
+if [ "$ARG" == "CHECK" ]; then
+  TCPHEX=`printf '%04X\n' "${TCP}"`
+  cat /proc/net/tcp |grep -q "^\s*[0-9]\+:\s*[0-9A-Za-z]\+:${TCPHEX}\s*[0-9A-Za-z]\+:[0-9A-Za-z]\+\s*0A\s*"
+  [ "$?" -eq 0 ] && exit 0 || exit 1
+elif [ "$ARG" == "INIT" ]; then
+  if [ -d "/usr/lib/systemd/system" ] && [ -f "${ConfigPath}/ocserv.service" ]; then
+    systemctl stop ocserv.service >/dev/null 2>&1
+    systemctl disable ocserv.service >/dev/null 2>&1
+    cp -rf "${ConfigPath}/ocserv.service" "/etc/systemd/system/ocserv.service"
+    chmod 755 "/etc/systemd/system/ocserv.service"
+    systemctl daemon-reload >/dev/null 2>&1
+    systemctl enable ocserv.service >/dev/null 2>&1
+  fi
+  touch "${ConfigPath}/ocpasswd"
+  openssl genrsa -out "${ConfigPath}/server.key.pem" 2048
+  openssl req -new -x509 -days 3650 -key "${ConfigPath}/server.key.pem" -out "${ConfigPath}/server.cert.pem" -subj "/C=/ST=/L=/O=/OU=/CN=$(GetAddress)"
+  chown -R root:root "${ConfigPath}"
+  chmod -R 755 "${ConfigPath}"
+  exit 0
+fi
+
+Ether=`ip route show default |sed 's/.*dev\s*\([0-9a-zA-Z]\+\).*/\1/g'`
+[ -n "$Ether" ] || exit 1
+
+[ -f "${ConfigPath}/group/NoRoute" ] && Address="$(GetAddress)" && [ -n "$Address" ] &&  sed -i "s/^no-route\s*=\s*.*\/255.255.255.255/no-route = ${Address}\/255.255.255.255/" "${ConfigPath}/group/NoRoute"
 
 IPTABLES "iptables -t nat -A POSTROUTING -o ${Ether} -j MASQUERADE"
 IPTABLES "iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu"
