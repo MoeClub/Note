e5a1cb0a1f5e72bfa0354f8732b84cb365a4be10
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-03-16 12:20:20 +0800

    Add files via upload

--- AnyConnect/build/client.sh
+++ AnyConnect/build/client.sh
@@ -2,13 +2,12 @@
 
 command -v openssl >>/dev/null 2>&1
 [ $? -ne 0 ] && echo "Not Found openssl" && exit 1
-command -v certtool >>/dev/null 2>&1
-[ $? -ne 0 ] && echo "Not Found certtool" && exit 1
 cd `dirname "$0"`
 
 export OrgName
 export GroupName
 export PASSWORD
+export INIT="0"
 
 
 while [[ $# -ge 1 ]]; do
@@ -28,6 +27,10 @@ while [[ $# -ge 1 ]]; do
       PASSWORD=`echo "$1" |sed 's/\s//g'`
       shift
       ;;
+    -i)
+      shift
+      INIT="1"
+      ;;
     *)
       echo -e "Usage:\n\tbash $0 -o <OrgName> -g <GroupName> -p <PASSWORD>\n"
       exit 1;
@@ -40,34 +43,20 @@ done
 
 
 if [ ! -f ./ca.cert.pem -o ! -f ./ca.key.pem ]; then
-  if [ ! -f ./ca.cfg ]; then
-    echo -e "cn = \"${OrgName} CA\"\norganization = \"${OrgName}\"\nserial = 1\nexpiration_days = 3650\nca\nsigning_key\ncert_signing_key\ncrl_signing_key\n" >./ca.cfg
-  fi
-  openssl genrsa -out ./ca.key.pem 2048
-  certtool --generate-privkey --outfile ./ca.key.pem
-  certtool --generate-self-signed --template ./ca.cfg --load-privkey ./ca.key.pem --outfile ./ca.cert.pem
+  openssl req -x509 -sha256 -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -days 3650 -subj "/C=/ST=/L=/OU=/O=/CN=${OrgName} CA" -addext "keyUsage=critical, keyCertSign, cRLSign" -outform PEM -keyout ./ca.key.pem -out ./ca.cert.pem  >/dev/null 2>&1
+  [ $? -ne 0 ] && echo "Generating CA Fail" && exit 1
   cp -rf ./ca.cert.pem ../ca.cert.pem
-  rm -rf ./ca.cfg
 fi
 
-if [ ! -f ../server.cert.pem -o ! -f ../server.key.pem ]; then
-  if [ ! -f ./server.cfg ]; then
-    echo -e "cn = \"${OrgName} CA\"\norganization = \"${OrgName}\"\nexpiration_days = -1\nsigning_key\nencryption_key\ntls_www_server\n" >./server.cfg
-  fi
-  openssl genrsa -out ../server.key.pem 2048
-  certtool --generate-certificate --load-privkey ../server.key.pem --load-ca-certificate ./ca.cert.pem --load-ca-privkey ./ca.key.pem --template ./server.cfg --outfile ../server.cert.pem
-  rm -rf ./server.cfg
+if [ "$INIT" == "1" ]; then
+  exit 0
 fi
 
-if [ ! -f ../dh.pem ]; then
-  certtool --generate-dh-params --outfile ../dh.pem
-fi
+openssl req -x509 -sha256 -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -days 3650 -subj "/C=/ST=/L=/OU=${GroupName}/O=/CN=${OrgName}.${GroupName}" -addext "keyUsage=critical, digitalSignature" -outform PEM -keyout ./user.key.pem -out ./user.cert.pem  >/dev/null 2>&1
+[ $? -ne 0 ] && echo "Generating Cert Fail" && exit 1
 
-echo -e "cn = \"${OrgName}.${GroupName}\"\nunit = \"${GroupName}\"\nexpiration_days = 3650\nsigning_key\ntls_www_client\n" >./user.cfg
-openssl genrsa -out ./user.key.pem 2048
-certtool --generate-certificate --load-privkey ./user.key.pem --load-ca-certificate ./ca.cert.pem --load-ca-privkey ./ca.key.pem --template ./user.cfg --outfile ./user.cert.pem
 cat ./ca.cert.pem >>./user.cert.pem
 openssl pkcs12 -export -inkey ./user.key.pem -in ./user.cert.pem -name "${OrgName}.${GroupName}" -certfile ./ca.cert.pem -caname "${OrgName} CA" -out "./${GroupName}.p12" -passout pass:$PASSWORD
 
 [ $? -eq '0' ] && echo -e "\nSuccess! \nGROUP\t\tPASSWORD\n${GroupName}\t\t$PASSWORD\n" || echo -e "\nFail! \n";
-rm -rf ./user.cert.pem ./user.key.pem ./user.cfg
+rm -rf ./user.cert.pem ./user.key.pem
