6bcef9f23eb6965c5d0b1a0fe7c77c4b88fdf6c6
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-05-23 14:44:02 +0800

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -9,7 +9,7 @@ LoopNum=${3:-20}
 ServerHost="$4"
 
 
-UserAgent="Mozilla/5.0"
+UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
 
 FileMode=0
 HostMode=0
