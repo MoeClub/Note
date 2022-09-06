397ba37b997f9dce6652a0bb9556cb7ccfdb3fbc
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-09-06 14:30:18 +0800

    Create install.sh

---
+++ qBittorrent/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+case `uname -m` in aarch64|arm64) ARCH="arm64";; x86_64|amd64) ARCH="amd64";; *) ARCH="";; esac
+[ -n "$ARCH" ] || exit 1
+
+systemctl stop qBittorrent.service >/dev/null 2>&1
+systemctl disable qBittorrent.service >/dev/null 2>&1
+rm -rf /usr/bin/qbittorrent
+
+URL="https://github.com/MoeClub/Note/raw/master/qBittorrent"
+
+wget --no-check-certificate -4 -qO- "${URL}/bin/qbittorrent_${ARCH}_v4.4.2_lt_v2.0.6.tar.xz" |tar -Jxv -C /usr/bin
+[ $? -eq 0 ] || exit 1
+
+strip /usr/bin/qbittorrent >/dev/null 2>&1
+chmod 777 /usr/bin/qbittorrent
+mkdir -p /home/qBittorrent/config
+
+wget --no-check-certificate -4 -qO- "${URL}/qBittorrent.conf" >/home/qBittorrent/config/qBittorrent.conf
+wget --no-check-certificate -4 -qO- "${URL}/qBittorrent.service" >/etc/systemd/system/qBittorrent.service
+
+systemctl daemon-reload >/dev/null 2>&1
+systemctl enable qBittorrent.service >/dev/null 2>&1
+systemctl start qBittorrent.service >/dev/null 2>&1
+systemctl status qBittorrent.service 
+
+
+
+
