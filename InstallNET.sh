831e378278cd4d7fc890213329c4b5234f8b91d8
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-09-19 15:20:12 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -613,6 +613,9 @@ $UNCOMP < /tmp/$NewIMG | cpio --extract --verbose --make-directories --no-absolu
 if [[ "$linux_relese" == 'debian' ]] || [[ "$linux_relese" == 'ubuntu' ]]; then
 cat >/tmp/boot/preseed.cfg<<EOF
 d-i debian-installer/locale string en_US
+d-i debian-installer/country string US
+d-i debian-installer/language string en
+
 d-i console-setup/layoutcode string us
 
 d-i keyboard-configuration/xkb-keymap string us
