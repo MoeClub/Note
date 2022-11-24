b5fbd34594e833423e8a5d9f93adfbd9965106bf
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-11-24 12:34:55 +0800

    Update navicat14.sh

--- Navicat/navicat14.sh
+++ Navicat/navicat14.sh
@@ -2,7 +2,10 @@
 
 Plist="${1:-com.navicat.NavicatPremium.plist}"
 
+defaults write "$Plist" SUSendProfileInfo -int 0;
 defaults write "$Plist" SUHasLaunchedBefore -int 0;
+defaults write "$Plist" SUEnableAutomaticChecks -int 0;
+defaults write "$Plist" didNAV16WelcomePageShow -int 1;
 defaults delete "$Plist" tableViewPreference;
 
 defaults read "$Plist" |grep '{' |grep -o '[0-9A-Z]\{32\}' |xargs -I {} defaults delete "$Plist" "{}"
