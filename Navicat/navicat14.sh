0908b178d68ac2800ed771515a9e469a7efc1b72
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-11-24 12:21:06 +0800

    Update navicat14.sh

--- Navicat/navicat14.sh
+++ Navicat/navicat14.sh
@@ -3,5 +3,7 @@
 Plist="${1:-com.navicat.NavicatPremium.plist}"
 
 defaults write "$Plist" SUHasLaunchedBefore -int 0;
+defaults delete "$Plist" tableViewPreference;
+
 defaults read "$Plist" |grep '{' |grep -o '[0-9A-Z]\{32\}' |xargs -I {} defaults delete "$Plist" "{}"
 find "$HOME/Library/Application Support/PremiumSoft CyberTech/Navicat CC/Navicat Premium" -type f -name ".*" -delete
