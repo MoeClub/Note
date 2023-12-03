397fb47ae911663946a361fd081b80ea3088400e
MoeClub <32381827+MoeClub@users.noreply.github.com>
2023-12-03 15:01:40 +0800

    Add files via upload

--- C8-660/network.sh
+++ C8-660/network.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-PORT=2
-MaxNum=120
-LOG="/tmp/network.log"
-BAND="78"
+# Lock PCI, If PCILock is empty, will lock first PCI.
+PCIStatic=1
+# PCILock="<PCI>,<RFCN>,<BAND>,<SCS>"
+PCILock=""
 # BAND="", BAND="78", BAND="1:78"
+BAND=""
+# Use SIM Card Index
+SIMCardIndex=1
 
 
 [ -e /bin/sendat ] || exit 1
+
+PORT=2
+MaxNum=120
+LOG="/tmp/network.log"
 
 function Now() {
   echo -ne `date '+[%Y/%m/%d %H:%M:%S']`
@@ -46,7 +53,7 @@ function Driver(){
 
 function NR5G(){
   rfBand=`/bin/sendat "$PORT" 'AT+QNWPREFCFG="rf_band"' |grep '+QNWPREFCFG:\s*"nr5g_band"' |cut -d',' -f2 |grep -o '[0-9A-Za-z:]*'`
-  band=${1:-$rfBand}
+  band="${1:-$rfBand}"
   echo "$(Now) Set NR5G ${band} ..." |tee -a "$LOG"
   /bin/sendat "$PORT" 'AT+QNWCFG="data_roaming",1'
   /bin/sendat "$PORT" 'AT+QNWPREFCFG="roam_pref",255'
@@ -55,11 +62,11 @@ function NR5G(){
   /bin/sendat "$PORT" "AT+QNWPREFCFG=\"nr5g_band\",${band}"
 }
 
-function Reset(){
+function Modem(){
   echo "$(Now) Reset Modem ..." |tee -a "$LOG"
   /bin/sendat "$PORT" 'AT+QSCLK=0,0'
   /bin/sendat "$PORT" 'AT+QMAPWAC=1'
-  /bin/sendat "$PORT" 'AT+QUIMSLOT=1'
+  /bin/sendat "$PORT" "AT+QUIMSLOT=${SIMCardIndex:-1}"
   [ $1 -gt 0 ] && /bin/sendat "$PORT" 'AT+CFUN=1,1'
   sleep 5
 }
@@ -68,12 +75,12 @@ function MPDN() {
   WaitAT || return 1
   
   echo "$(Now) Empty MPDN ..." |tee -a "$LOG"
-  /bin/sendat "$PORT" 'AT+QMAP="mPDN_rule",0'
+  /bin/sendat "$PORT" 'AT+QMAP="MPDN_rule",0'
   sleep 5
 
   WaitAT || return 1
   for i in $(seq 1 $MaxNum); do
-    result=`/bin/sendat "$PORT" 'AT+QMAP="mPDN_rule"'`
+    result=`/bin/sendat "$PORT" 'AT+QMAP="MPDN_rule"'`
     echo "$result" |grep -q '0,0,0,0,0'
     [ $? -eq 0 ] && break || sleep 1
     echo "$result" |grep -q '0,1,0,1,1'
@@ -82,12 +89,12 @@ function MPDN() {
 
   WaitAT || return 1
   echo "$(Now) Reset MPDN ..." |tee -a "$LOG"
-  /bin/sendat "$PORT" 'AT+QMAP="mPDN_rule",0,1,0,1,1,"FF:FF:FF:FF:FF:FF"'
+  /bin/sendat "$PORT" 'AT+QMAP="MPDN_rule",0,1,0,1,1,"FF:FF:FF:FF:FF:FF"'
   sleep 5
 
   WaitAT || return 1
   for i in $(seq 1 $MaxNum); do
-    result=`/bin/sendat "$PORT" 'AT+QMAP="mPDN_rule"'`
+    result=`/bin/sendat "$PORT" 'AT+QMAP="MPDN_rule"'`
     echo "$result" |grep -q '0,1,0,1,1'
     [ $? -eq 0 ] && break || sleep 1
     echo "$result" |grep -q '0,0,0,0,0'
@@ -95,9 +102,9 @@ function MPDN() {
   done
 }
 
-function CheckIPv4() {
+function WaitIPv4() {
   WaitAT || return 1
-  echo "$(Now) Check IPv4 ..." |tee -a "$LOG"
+  echo "$(Now) Wait IPv4 ..." |tee -a "$LOG"
   maxNum=$(($MaxNum/4))
   for i in $(seq 1 $maxNum); do
     ipv4=`/bin/sendat "$PORT" 'AT+CGPADDR=1' |grep '+CGPADDR:' |cut -d',' -f2 |grep -o '[0-9\.]*'`
@@ -109,6 +116,15 @@ function CheckIPv4() {
 
 function ReloadIf() {
   [ -e /sbin/ifup ] || return 1
+  echo "$(Now) Check Interface ..." |tee -a "$LOG"
+  mode="${1:-0}"
+  if [ "$mode" -eq 0 ]; then
+    ipv4=`/bin/sendat "$PORT" 'AT+CGPADDR=1' |grep '+CGPADDR:' |cut -d',' -f2 |grep -o '[0-9\.]*'`
+    [ -n "$ipv4" ] || ipv4="0.0.0.0"
+    ipv4If=`ubus call network.interface.wan status |grep '"address":' |cut -d'"' -f4 |grep -o '[0-9\.]*'`
+    [ -n "$ipv4If" ] || ipv4If="0.0.0.0"
+    [ "$ipv4" == "$ipv4If" ] && return 0
+  fi
   echo "$(Now) Reload Interface ..." |tee -a "$LOG"
   /sbin/ifup wan
   /sbin/ifup wan6
@@ -117,7 +133,37 @@ function ReloadIf() {
 
 # /bin/sendat 3 'AT+QSCAN=2,1'
 # /bin/sendat 3 'AT'
-# /bin/sendat 3 'AT+QNWCFG="nr5g_earfcn_lock"'
+
+function LockNR5G() {
+  # pci:rfcn:band:scs
+  echo "$(Now) NR5G Lock ..." |tee -a "$LOG"
+  cell=`/bin/sendat "$PORT" 'AT+QENG="servingcell"'|grep '+QENG:' |cut -d',' -f8,10,16,11`
+  echo "$(Now) NR5G Cell: $cell" |tee -a "$LOG"
+  [ -n "$cell" ] || return 1
+  lock="${1:-$cell}"
+  echo "$(Now) NR5G Lock: $lock" |tee -a "$LOG"
+  pci=`echo "$lock" |cut -d',' -f1`
+  rfcn=`echo "$lock" |cut -d',' -f2`
+  band=`echo "$lock" |cut -d',' -f3`
+  scs=`echo "$lock" |cut -d',' -f4`
+  [ "$lock" != "$cell" ] && {
+      meas=`/bin/sendat "$PORT" 'AT+QNWCFG="nr5g_meas_info"' |grep '+QNWCFG:' |cut -d',' -f4,3`
+      echo "$meas" |grep -q "^${rfcn},${pci}$"
+      [ $? -eq 0 ] || {
+          echo "$(Now) NR5G Lock: NotFoundPCI" |tee -a "$LOG"
+          return 1
+      }
+  }
+  scsHz="$(($((2**${scs}))*15))"
+  /bin/sendat "$PORT" "AT+QNWLOCK=\"common/5g\",${pci},${rfcn},${scsHz},${band}" |grep -q "OK"
+  [ $? -eq 0 ] && {
+      echo "$(Now) NR5G: N${band}:${rfcn}:${pci}" |tee -a "$LOG"
+      return 0
+  } || {
+      echo "$(Now) NR5G Lock: Fail" |tee -a "$LOG"
+      return 1
+  }
+}
 
 echo "$(Now) START" |tee -a "$LOG"
 
@@ -130,12 +176,15 @@ for i in $(seq 1 $MaxNum); do
       Driver
       NR5G "$BAND"
     }
-    Reset "$n"
+    Modem "$n"
     WaitSIM
   }
   
+  [ "$PCIStatic" -gt 0 ] && LockNR5G "$PCILock"
   MPDN || continue
-  CheckIPv4 && ReloadIf && break
+  WaitIPv4 || continue
+  
+  ReloadIf && break
 done
 
 echo "$(Now) FINISH" |tee -a "$LOG"
