1495dfd59e5c8b1d5fa31078d94416b877a6b960
MoeClub <32381827+MoeClub@users.noreply.github.com>
2023-12-04 19:31:40 +0800

    Add files via upload

--- C8-660/notice.sh
+++ C8-660/notice.sh
@@ -5,6 +5,9 @@ MaxNum=30
 LOG="/tmp/notice.log"
 # LOG="/dev/null"
 
+BarkURL=""
+
+
 Device="/dev/ttyUSB${PORT}"
 
 function Now() {
@@ -45,8 +48,35 @@ function ReloadWAN() {
 
 function NewSMS() {
   echo "$(Now) New SMS ..." |tee -a "$LOG"
-  data=`sms_tool -f '%s' -j -u recv`
-  echo "$data"
+  data=`sms_tool -f '%Y/%m/%d %H:%M:%S' -j -u recv`
+  SMSWithBark "$data"
+}
+
+function SMSWithBark() {
+  echo "$(Now) SMS With Bark ..." |tee -a "$LOG"
+  data="${1:-}"
+  [ -n "$data" ] || return 1
+  length=`echo "$data" |jq '.msg |length'`
+  [ $length -gt 0 ] || return 1
+  i=0
+  while [ $i -lt $length ]; do
+    item=`echo "$data" |jq -c ".msg[$i]"`
+    echo "$(Now) SMS: $item" |tee -a "$LOG"
+    sender=`echo "$item" |jq -r ".sender"`
+    timestamp=`echo "$item" |jq -r ".timestamp"`
+    content=`echo "$item" |jq -r ".content"`
+    title="[${timestamp}] ${sender}"
+    BarkService "$title" "$content"
+    i=$(($i+1))
+  done
+}
+
+function BarkService() {
+  [ -n "$BarkURL" ] || return 1
+  title=`echo "$1" |jq -sRr @uri`
+  body=`echo "$2" |jq -sRr @uri`
+  url="${BarkURL%/}/${title}/${body}"
+  curl -ksSL --connect-timeout 5 -X GET "${url}" >/dev/null 2>&1 &
 }
 
 while true; do
@@ -57,12 +87,12 @@ while true; do
   while IFS= read -r line; do
     echo -ne "$line" |grep -q '+CMTI:\s*"ME"\|+C5GREG:\s*[15]\|+CREG:\s*[15]'
     [ $? -eq 0 ] || continue
-    var=`echo -ne "$line" |cut -d':' -f1 |grep -o '[0-9A-Za-z]*'`
+    var=`echo -ne "${line%%:*}"`
     case $var in
-      CMTI)
+      +CMTI)
         NewSMS;
         ;;
-      CREG|C5GREG)
+      +CREG|+C5GREG)
         ReloadWAN;
         ;;
       *)
