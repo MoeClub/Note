f4d7ff5bd0d32aec32b514661471d539272b3904
MoeClub <32381827+MoeClub@users.noreply.github.com>
2024-06-05 11:14:52 +0800

    Update acme.sh

--- ACME/acme.sh
+++ ACME/acme.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# /etc/crontab
-# 0 4 * * 3 root bash /full/path/to/acme.sh 1>/dev/null 2>&1 &
-
 DOMAIN=()
 DOMAIN+=("moeclub.org,*.moeclub.org")
 # DOMAIN+=("moeclub.org,*.moeclub.org")
@@ -15,5 +12,11 @@ for domain in "${DOMAIN[@]}"; do
   _domain="${domain};"
   domain=`echo "${_domain}" |cut -d';' -f1`
   sub=`echo "${_domain}" |cut -d';' -f2`
-  python3 ./acme.py -s "${s}" -d "${domain}" -sub "${sub}"
+  if [ "${s}" == "letsencrypt" ]; then
+    python3 ./acme.py -s "${s}" -ecc -d "${domain}" -sub "${sub}"
+  else
+    python3 ./acme.py -s "${s}" -d "${domain}" -sub "${sub}"
+  fi
 done
+
+
