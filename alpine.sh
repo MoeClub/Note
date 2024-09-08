fcb0c0e4d6f883247ca4b3a17e2f2186b931ae9a
MoeClub <32381827+MoeClub@users.noreply.github.com>
2024-09-08 20:30:30 +0800

    Create alpine.sh

---
+++ alpine.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PORT="${1:-22}"
+PASS="${2:-Vicer}"
+
+echo "alpine" >/etc/hostname
+echo "root:${PASS}" |chpasswd root
+sed -i "s/^#\?Port.*/Port $PORT/g" /etc/ssh/sshd_config;
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+
+[ -e /usr/share/zoneinfo/Asia/Shanghai ] && {
+  cp -rf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+  echo "Asia/Shanghai" > /etc/timezone
+}
+
+for usr in `cat /etc/passwd |cut -d':' -f1,6`; do
+  u=`echo "$usr" |cut -d':' -f1`
+  h=`echo "$usr" |cut -d':' -f2`
+  echo "$h" |grep -q '^/home' && {
+  	deluser "$u" 2>/dev/null
+  	rm -rf "$h" 2>/dev/null
+  }
+done
+
