f923cb267a504ff272abcd4a9692317a6391f78b
MoeClub <32381827+MoeClub@users.noreply.github.com>
2025-05-16 17:04:29 +0800

    Update bdp.sh

--- bdp.sh
+++ bdp.sh
@@ -5,6 +5,9 @@ RTT="${2:-60}"           # ms
 BDP=`echo "${Bandwidth} ${RTT}" |awk '{printf "%d", ($1 * $2) * ((1024 * 1024) / (1000 * 8))}' 2>/dev/null`
 [ -n "$BDP" ] && [ "$BDP" -gt 0 ] || exit 1
 
+sysctl -w net.core.optmem_max="${BDP}"
+sysctl -w net.core.rmem_default="${BDP}"
+sysctl -w net.core.wmem_default="${BDP}"
 sysctl -w net.core.rmem_max="${BDP}"
 sysctl -w net.core.wmem_max="${BDP}"
 sysctl -w net.ipv4.tcp_rmem="4096 87380 ${BDP}"
