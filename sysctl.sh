bc959a8702ff0e2a701c88baae1a396d4883a03f
MoeClub <32381827+MoeClub@users.noreply.github.com>
2025-05-19 22:39:25 +0800

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -34,6 +34,7 @@ vm.swappiness = 10
 
 net.netfilter.nf_conntrack_max = 1048576
 
+net.core.default_qdisc = fq_codel
 net.core.somaxconn = 1048576
 net.core.optmem_max = 7864320
 net.core.rmem_max = 7864320
@@ -41,30 +42,34 @@ net.core.wmem_max = 7864320
 net.core.rmem_default = 7864320
 net.core.wmem_default = 7864320
 net.core.netdev_max_backlog = 1048576
-net.core.default_qdisc = fq_codel
 net.core.busy_poll = 0
 net.core.busy_read = 0
 
+net.ipv4.ip_forward = 1
 net.ipv4.tcp_congestion_control = bbr
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+net.ipv4.udp_mem = 32768 49152 65536
 net.ipv4.tcp_mem = 32768 49152 65536
 net.ipv4.tcp_rmem = 4096 87380 7864320
 net.ipv4.tcp_wmem = 4096 16384 7864320
 net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_syn_retries = 3
 net.ipv4.tcp_synack_retries = 2
-net.ipv4.tcp_max_syn_backlog = 1048576
+net.ipv4.tcp_max_syn_backlog = 4096
 net.ipv4.tcp_fin_timeout = 8
 net.ipv4.tcp_keepalive_intvl = 32
 net.ipv4.tcp_keepalive_probes = 3
-net.ipv4.tcp_keepalive_time = 900
+net.ipv4.tcp_keepalive_time = 600
 net.ipv4.tcp_retries1 = 3
 net.ipv4.tcp_retries2 = 5
 net.ipv4.tcp_no_metrics_save = 1
 net.ipv4.tcp_timestamps = 1
 net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_moderate_rcvbuf = 1
 net.ipv4.tcp_slow_start_after_idle = 0
-net.ipv4.ip_forward = 1
-
+net.ipv4.tcp_notsent_lowat = 32768
+net.ipv4.tcp_mtu_probing = 1
 net.ipv4.tcp_fastopen = 0
 net.ipv4.tcp_fack = 1
 net.ipv4.tcp_sack = 1
@@ -72,14 +77,13 @@ net.ipv4.tcp_dsack = 1
 net.ipv4.tcp_ecn = 0
 net.ipv4.tcp_ecn_fallback = 1
 
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
 
 net.ipv6.conf.all.disable_ipv6 = 0
 net.ipv6.conf.all.forwarding = 1
 net.ipv6.conf.all.accept_ra = 2
 net.ipv6.conf.all.proxy_ndp = 1
-
-
-
 
 
 EOF
