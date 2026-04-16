3a3c8be0f9732cd7c3ea20b4363e4eeaa1f84728
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-16 21:59:14 +0800

    Update update.sh

--- AnyConnect/build/update.sh
+++ AnyConnect/build/update.sh
@@ -1,27 +1,52 @@
 #!/bin/sh
 
-ver="${1:-1.4.1}"
+oVer="${1:-0}"
+dVer="${2:-0}"
 
 case `uname -m` in aarch64|arm64) arch="aarch64";; x86_64|amd64) arch="x86_64";; *) arch="";; esac
 [ -n "$arch" ] || exit 1
 
-tarfile=`mktemp -u`
-trap "rm -rf ${tarfile}" EXIT
-wget -qO "${tarfile}" "https://github.com/MoeClub/Note/raw/refs/heads/master/AnyConnect/build/ocserv_${arch}_v${ver}.tar.gz"
-[ $? -eq 0 ] || exit 1
-md5sum "${tarfile}"
+if [ "$oVer" != "0" ]; then
+  ocservFile="ocserv_${arch}_v${oVer}.tar.gz"
+  [ -f "./${ocservFile}" ] || {
+    wget -qO "${ocservFile}" "https://github.com/MoeClub/Note/raw/refs/heads/master/AnyConnect/build/ocserv_${arch}_v${oVer}.tar.gz"
+    trapFile="${ocservFile}"
+    trap "rm -rf ${trapFile}" EXIT
+    [ $? -eq 0 ] || exit 1
+  }
 
-rm -rf /usr/bin/occtl
-rm -rf /usr/bin/ocpasswd
-rm -rf /usr/bin/ocserv-fw
-rm -rf /usr/libexec/ocserv-fw
-rm -rf /usr/sbin/ocserv
-rm -rf /usr/sbin/ocserv-worker
-rm -rf /usr/share/man/man8/occtl.8
-rm -rf /usr/share/man/man8/ocpasswd.8
-rm -rf /usr/share/man/man8/ocserv.8
+  rm -rf /usr/bin/occtl
+  rm -rf /usr/bin/ocpasswd
+  rm -rf /usr/bin/ocserv-fw
+  rm -rf /usr/libexec/ocserv-fw
+  rm -rf /usr/sbin/ocserv
+  rm -rf /usr/sbin/ocserv-worker
+  rm -rf /usr/share/man/man8/occtl.8
+  rm -rf /usr/share/man/man8/ocpasswd.8
+  rm -rf /usr/share/man/man8/ocserv.8
 
-tar --overwrite -xvf "${tarfile}" -C /
+  tar --overwrite -xvf "${ocservFile}" -C /
 
-systemctl restart ocserv
-ocserv -v
+  systemctl restart ocserv
+  ocserv -v
+if
+
+if [ "$dVer" != "0" ]; then
+  dnsmasqFile="dnsmasq_${arch}_v${dVer}.tar.gz"
+  [ -f "./${dnsmasqFile}" ] || {
+    wget -qO "${dnsmasqFile}" "https://github.com/MoeClub/Note/raw/refs/heads/master/AnyConnect/build/dnsmasq_${arch}_v${dVer}.tar.gz"
+    trapFile="${trapFile} ${dnsmasqFile}"
+    trap "rm -rf ${trapFile}" EXIT
+    [ $? -eq 0 ] || exit 1
+  }
+
+  rm -rf /usr/sbin/ocserv
+  rm -rf /usr/share/man/man8/dnsmasq.8
+
+  tar --overwrite -xvf "${dnsmasqFile}" -C /
+
+  systemctl restart dnsmasq
+  dnsmasq -v
+if
+
+
