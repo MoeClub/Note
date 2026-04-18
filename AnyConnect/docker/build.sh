a568bee7ba3233fc306e1e4a367c4290854e01f3
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-18 14:42:34 +0800

    Update build.sh

--- AnyConnect/docker/build.sh
+++ AnyConnect/docker/build.sh
@@ -12,6 +12,12 @@ docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${ocVer}" "${dnsVer}"
 docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "ocserv:${ocVer}"
 docker tag "ocserv:${ocVer}" ocserv:latest
 docker rm -f "${dockerName}" >/dev/null 2>&1;
+
+docker tag ocserv:latest ocserv/ocserv:latest
+docker push ocserv/ocserv:latest
+docker tag ocserv:latest ocserv/ocserv:${ocVer}
+docker push ocserv/ocserv:${ocVer}
+
 # docker run --privileged --rm -it -p 8123:443 ocserv /bin/sh
 # docker ps -aq |xargs docker rm -f
 # docker images -aq |xargs docker rmi -f
