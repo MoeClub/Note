7d30e1089d9ccb705592f14949bb8587acd0d94a
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-21 15:07:40 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 dockerVer="${1:-20.10.24}"
+buildxVer="${1:-0.10.4}"
+composeVer="${1-2.39.2}"
 
 rm -rf /usr/bin/docker-init
 rm -rf /usr/bin/containerd
@@ -14,6 +16,7 @@ rm -rf /usr/bin/containerd-shim-runc-v2
 rm -rf /etc/systemd/system/docker.service
 rm -rf /var/lib/docker
 rm -rf /etc/docker
+rm -rf /usr/local/lib/docker
 ps -C dockerd -o pid= |xargs kill -9 >/dev/null 2>&1
 [ "$dockerVer" == "0" ] && exit 0
 
@@ -44,6 +47,12 @@ RestartSec=1s
 WantedBy=multi-user.target
 
 EOF
+
+mkdir -p /usr/local/lib/docker/cli-plugins
+[ -n "${buildxVer}" ] && wget --no-check-certificate -4 -qO "/usr/local/lib/docker/cli-plugins/docker-buildx" "https://github.com/docker/buildx/releases/download/v${buildxVer}/buildx-v${buildxVer}.linux-${arch}" 
+[ -n "${composeVer}" ] && wget --no-check-certificate -4 -qO "/usr/local/lib/docker/cli-plugins/docker-compose" "https://github.com/docker/compose/releases/download/v${composeVer}/compose-v${composeVer}.linux-${arch}" 
+
+chmod -R 755 /usr/local/lib/docker/cli-plugins
 
 systemctl disable docker 2>/dev/null
 systemctl daemon-reload
