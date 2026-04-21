0d3bbbf9f95854391be906043f96ef511a07fe62
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-21 14:06:00 +0800

    Update ocserv_musl.sh

--- AnyConnect/build/ocserv_musl.sh
+++ AnyConnect/build/ocserv_musl.sh
@@ -5,10 +5,10 @@
 # docker exec -it alpine /bin/sh
 
 apk update
-apk add wget xz sed openssl gcc coreutils patch file autoconf automake make pkg-config linux-headers gperf musl-dev gnutls-dev gnutls-utils 
+apk add wget xz sed openssl gcc coreutils patch file autoconf automake make linux-headers gperf musl-dev gnutls-dev gnutls-utils meson
 
 
-VERSION_OCSERV="1.4.1"
+VERSION_OCSERV="1.4.2"
 VERSION_DNSMASQ="2.92"
 VERSION_LIBEV="4.33"
 VERSION_LZ4="1.10.0"
@@ -37,6 +37,56 @@ function musl_cross(){
 	[ -f "$HOME/.bashrc" ] && sed -i "s/^PATH=.*/PATH=${newPATH////\\/}/" "$HOME/.bashrc"
 	[ -f "/etc/profile" ] && sed -i "s/^export PATH=.*/export PATH=${newPATH////\\/}/" "/etc/profile"
 	echo "export PATH=${newPATH}"
+}
+
+
+function meson_cross(){
+  pkgPath=`which pkg-config 2>/dev/null`
+  ccPath=`which "$CC" 2>/dev/null`
+  [ -n "$ccPath" ] && [ -n "$pkgPath" ] || return 1
+  ccDir=`dirname "$ccPath"`
+  machine=`"$CC" -dumpmachine`
+  arch="${machine%%-*}"
+  cpu_family="${arch}"
+  cpu="${arch}"
+  endian="little"
+
+  case "$arch" in arm64) cpu_family="aarch64"; cpu="aarch64" ;; i?86) cpu_family="x86" ;; *) ;; esac
+  case "$machine" in *linux*) system="linux" ;; *darwin*) system="darwin" ;; *mingw*|*windows*) system="windows" ;; *) ;; esac
+
+  [ -n "$system" ] || return 1
+  corss=`mktemp -u /tmp/meson.cross.XXXXXXXX`
+  {
+    echo '[binaries]'
+    [ -e "${ccDir}/${cpu_family}-linux-musl-gcc" ] && echo "c = '${ccDir}/${cpu_family}-linux-musl-gcc'"
+    [ -e "${ccDir}/${cpu_family}-linux-musl-g++" ] && echo "cpp = '${ccDir}/${cpu_family}-linux-musl-g++'"
+    [ -e "${ccDir}/${cpu_family}-linux-musl-ar" ] && echo "ar = '${ccDir}/${cpu_family}-linux-musl-ar'"
+    [ -e "${ccDir}/${cpu_family}-linux-musl-strip" ] && echo "strip = '${ccDir}/${cpu_family}-linux-musl-strip'"
+    [ -e "${ccDir}/${cpu_family}-linux-musl-objcopy" ] && echo "objcopy = '${ccDir}/${cpu_family}-linux-musl-objcopy'"
+    [ -e "${ccDir}/${cpu_family}-linux-musl-objdump" ] && echo "objdump = '${ccDir}/${cpu_family}-linux-musl-objdump'"
+    [ -e "${ccDir}/${cpu_family}-linux-musl-ld" ] && echo "ld = '${ccDir}/${cpu_family}-linux-musl-ld'"
+    echo "pkg-config = '${pkgPath}'"
+    echo
+    echo '[host_machine]'
+    echo "system = '${system}'"
+    echo "cpu_family = '${cpu_family}'"
+    echo "cpu = '${cpu}'"
+    echo "endian = '${endian}'"
+    echo
+    echo '[properties]'
+    echo 'needs_exe_wrapper = true'
+    echo "sys_root = '/usr/local/cross/${cpu_family}'"
+    echo "pkg_config_libdir = ['/usr/local/cross/${cpu_family}/lib/pkgconfig']"
+    echo
+    echo '[built-in options]'
+    echo 'prefer_static = true'
+    echo "default_library = 'static'"
+    echo "c_args = ['--sysroot=/usr/local/cross/${cpu_family}', '-I/usr/local/cross/${cpu_family}/include']"
+    echo "cpp_args = ['--sysroot=/usr/local/cross/${cpu_family}', '-I/usr/local/cross/${cpu_family}/include']"
+    echo "c_link_args = ['--sysroot=/usr/local/cross/${cpu_family}', '-L/usr/local/cross/${cpu_family}/lib', '-static']"
+    echo "cpp_link_args = ['--sysroot=/usr/local/cross/${cpu_family}', '-L/usr/local/cross/${cpu_family}/lib', '-static']"
+  } > "${corss}"
+  echo "${corss}"
 }
 
 # libev
@@ -84,7 +134,7 @@ function build_libseccomp(){
 # lz4
 function build_lz4(){
 	ARCH="${1:-x86_64}"
-	TMP=`mktemp -d`; # TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
+	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
 	wget --no-check-certificate -qO- "https://github.com/lz4/lz4/archive/refs/tags/v${VERSION_LZ4}.tar.gz" |tar -xz -C "$TMP" --strip-components=1
 	cd "$TMP"
 	CC="${ARCH}-linux-musl-gcc" \
@@ -177,13 +227,13 @@ function build_gnutls(){
 	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
---host="${ARCH}-linux-musl" \
---prefix="/usr/local/cross/${ARCH}" \
---enable-static=yes --enable-shared=no \
---with-included-libtasn1 --with-included-unistring \
---without-brotli --without-zstd --without-zlib \
---without-p11-kit --without-tpm --without-tpm2 \
---disable-doc --disable-tools --disable-cxx --disable-tests --disable-nls --disable-libdane --disable-gost --disable-guile --disable-rpath
+	--host="${ARCH}-linux-musl" \
+	--prefix="/usr/local/cross/${ARCH}" \
+	--enable-static=yes --enable-shared=no \
+	--with-included-libtasn1 --with-included-unistring \
+	--without-brotli --without-zstd --without-zlib \
+	--without-p11-kit --without-tpm --without-tpm2 \
+	--disable-doc --disable-tools --disable-cxx --disable-tests --disable-nls --disable-libdane --disable-gost --disable-guile --disable-rpath
 	[ $? -eq 0 ] || return 1
 	make -j`nproc`
 	[ $? -eq 0 ] || return 1
@@ -285,7 +335,7 @@ EOF
 
 function build_ocserv(){
 	ARCH="${1:-x86_64}"
-	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
+	TMP=`mktemp -d`; # TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
 	wget --no-check-certificate -qO- "ftp://ftp.infradead.org/pub/ocserv/ocserv-${VERSION_OCSERV}.tar.xz" |tar -xJ -C "$TMP" --strip-components=1
 	cd "$TMP"
 	sed -i 's/#define DEFAULT_CONFIG_ENTRIES 96/#define DEFAULT_CONFIG_ENTRIES 200/' src/vpn.h
@@ -293,37 +343,51 @@ function build_ocserv(){
 	sed -i 's/^#define WORKER_MAINTENANCE_TIME .*/#define WORKER_MAINTENANCE_TIME (4.)/' src/worker-vpn.c
 	sed -i 's/^#define PERIODIC_CHECK_TIME .*/#define PERIODIC_CHECK_TIME 3/' src/worker-vpn.c
 
-	autoreconf -fvi
-	
-	CC="${ARCH}-linux-musl-gcc" \
-	CXX="${ARCH}-linux-musl-g++" \
-	LIBREADLINE_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
-	LIBREADLINE_LIBS="-L/usr/local/cross/${ARCH}/lib -lreadline" \
-	LIBSECCOMP_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
-	LIBSECCOMP_LIBS="-L/usr/local/cross/${ARCH}/lib -lseccomp" \
-	LIBNETTLE_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
-	LIBNETTLE_LIBS="-L/usr/local/cross/${ARCH}/lib -lgmp -lnettle -lhogweed" \
-	LIBGNUTLS_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
-	LIBGNUTLS_LIBS="-L/usr/local/cross/${ARCH}/lib -lgnutls -lgmp -lnettle -lhogweed -lidn2" \
-	LIBLZ4_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
-	LIBLZ4_LIBS="-L/usr/local/cross/${ARCH}/lib -llz4" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
-	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -s -w -static -no-pie" \
-  ac_cv_file__proc_self_exe=yes \
-	./configure \
---host="${ARCH}-linux-musl" \
---prefix="/usr" \
---with-local-talloc \
---disable-dependency-tracking \
---without-root-tests --without-docker-tests --without-nuttcp-tests --without-tun-tests \
---without-protobuf --without-maxmind --without-geoip --without-liboath --without-pam --without-radius --without-utmp --without-http-parser --without-gssapi --without-pcl-lib --without-libwrap --without-llhttp
-
-	[ $? -eq 0 ] || return 1
-	make -j`nproc`
-	[ $? -eq 0 ] || return 1
 	TARGET=`mktemp -d`; TRAPRM="${TRAPRM} ${TARGET}"; trap "rm -rf ${TRAPRM# }" EXIT
-	make DESTDIR="${TARGET}" install
-	[ $? -eq 0 ] || return 1
+  if [ -f "meson.build" ]; then
+    corss=`CC="${ARCH}-linux-musl-gcc" meson_cross`
+    echo "cross: ${corss}"
+    PKG_CONFIG_LIBDIR="/usr/local/cross/${ARCH}/lib/pkgconfig" meson setup build \
+    --prefix "/usr" --cross-file "$corss" \
+    -Dlocal-talloc=true -Dlocal-llhttp=true -Dlocal-protobuf=true -Dlocal-pcl=true \
+    -Droot-tests=false -Dtun-tests=false -Dkerberos-tests=false -Dradius=disabled -Dgssapi=disabled -Dliboath=disabled -Dlibnl=disabled -Dmaxmind=disabled -Dgeoip=disabled -Dsystemd=disabled \
+    -Danyconnect-compat=enabled -Dcompression=enabled -Dseccomp=enabled -Dlz4=enabled
+    [ $? -eq 0 ] || return 1
+    meson compile -C build
+    [ $? -eq 0 ] || return 1
+    DESTDIR="${TARGET}" meson install -C build
+    [ $? -eq 0 ] || return 1
+  else
+	  autoreconf -fvi
+	  CC="${ARCH}-linux-musl-gcc" \
+	  CXX="${ARCH}-linux-musl-g++" \
+	  LIBREADLINE_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
+	  LIBREADLINE_LIBS="-L/usr/local/cross/${ARCH}/lib -lreadline" \
+	  LIBSECCOMP_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
+	  LIBSECCOMP_LIBS="-L/usr/local/cross/${ARCH}/lib -lseccomp" \
+	  LIBNETTLE_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
+	  LIBNETTLE_LIBS="-L/usr/local/cross/${ARCH}/lib -lgmp -lnettle -lhogweed" \
+	  LIBGNUTLS_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
+	  LIBGNUTLS_LIBS="-L/usr/local/cross/${ARCH}/lib -lgnutls -lgmp -lnettle -lhogweed -lidn2" \
+	  LIBLZ4_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
+	  LIBLZ4_LIBS="-L/usr/local/cross/${ARCH}/lib -llz4" \
+	  CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
+	  LDFLAGS="-L/usr/local/cross/${ARCH}/lib -s -w -static -no-pie" \
+    ac_cv_file__proc_self_exe=yes \
+	  ./configure \
+	    --host="${ARCH}-linux-musl" \
+	    --prefix="/usr" \
+	    --with-local-talloc \
+	    --disable-dependency-tracking \
+	    --without-root-tests --without-docker-tests --without-nuttcp-tests --without-tun-tests \
+	    --without-protobuf --without-maxmind --without-geoip --without-liboath --without-pam --without-radius --without-utmp --without-http-parser --without-gssapi --without-pcl-lib --without-libwrap --without-llhttp
+	  [ $? -eq 0 ] || return 1
+	  make -j`nproc`
+	  [ $? -eq 0 ] || return 1
+	  make DESTDIR="${TARGET}" install
+	  [ $? -eq 0 ] || return 1
+  fi
+
 	cd "${TARGET}"
 	FILE="/mnt/ocserv_${ARCH}_v${VERSION_OCSERV}.tar.gz"
 	[ -f "${FILE}" ] && rm -rf "${FILE}"
@@ -377,10 +441,11 @@ function build() {
 }
 
 
-for arch in "x86_64" "aarch64"; do
+for arch in "aarch64" "x86_64"; do
 	eval `musl_cross "${arch}"`
 	build "${arch}"
 	[ "$?" -ne 0 ] && exit 1
+  break
 done
 
 for tarpkg in `echo "${TARPKG# }"`; do
